443368463f254b79f23c6f88525863e082f5b07f
12Imix34 <137943245+Imixxxx@users.noreply.github.com>
2025-06-03 12:57:13 -0400

    Update game.js

--- game.js
+++ game.js
@@ -28,15 +28,15 @@ const broadcastBtn = document.getElementById('broadcast-btn');
 
 // autojoin code
 const autojoin = document.getElementById('autojoin')
-const localAutojoin = (localStorage.getItem('clashdot-autojoin') === 'true')
-if (localAutojoin) {
-  const username = localStorage.getItem('clashdot-username')
-  if (username) {
-    usernameInput.value = username
+const username = localStorage.getItem('clashdot-username')
+if (username) {
+  usernameInput.value = username
+  const localAutojoin = (localStorage.getItem('clashdot-autojoin') === 'true')
+  if (localAutojoin) {
     autojoin.checked = true
     const rawName = (localStorage.getItem('clashdot-isdev') === 'true') ? `${username}#1627` : username;
     console.log(rawName)
-    //start(rawName)
+  //start(rawName)
   }
 }
 
